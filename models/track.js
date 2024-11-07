e516b0be93e466f335beaed0cb59135ef66cabee
CinqueaS <cinquea.stafford@gmail.com>
2024-11-07 16:18:19 -0500

    CRUD complete

---
+++ models/track.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const trackSchema = mongoose.Schema({
+    title: {
+      type: String,
+      required: true,
+    },
+    artist: {
+      type: String,
+      required: true,
+    },
+    link: {
+        type: String,
+        required: true
+    }
+  });
+
+  const Track = mongoose.model('Track', trackSchema);
+  
+  module.exports = Track;
